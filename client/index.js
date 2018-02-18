ee6332884d1da8aa3eae55ae818a5722efe59fad
igorr <nimbik@gmail.com>
2018-02-18 18:21:09 +1300

    Added react-route

--- client/index.js
+++ client/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Router, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import SongList from './components/SongList'
+import SongList from './components/SongList';
+import App from './components/App';
+
  
 
 const client = new ApolloClient({
@@ -15,7 +18,12 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList/>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}> 
+          <IndexRoute component={SongList}/> 
+        </Route>
+        <Route path="/" component={App}> </Route>
+      </Router>
     </ApolloProvider>
   )
 };
