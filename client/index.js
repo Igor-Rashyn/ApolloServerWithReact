37bf61b818bac59698ff779a29cd39aabe2d0a39
igorr <nimbik@gmail.com>
2018-02-18 18:07:55 +1300

    added few dependencies for apollo server

--- client/index.js
+++ client/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import SongList from './components/SongList'
+ 
 
-
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+});
 
 const Root = () => {
   return (
